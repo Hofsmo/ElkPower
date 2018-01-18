2144a47cfd12bd98f240eee1c38232a0cdb8d396
Sigurd Hofsmo Jakobsen <sigurd.h.jakobsen@ntnu.no>
2018-01-18 18:23:41 +0100

    Added travis and tox support. This is a test commit to see if it actually works.

---
+++ setup.py
@@ -0,0 +1,7 @@
+"""Setup script for PyDy."""
+from setuptools import setup
+setup(name='pydy',
+      version='0.1',
+      description="Power system dynamic simulation.",
+      py_modules=['pydy']
+     )
---
+++ test/test_grid.py
@@ -0,0 +1,4 @@
+"""Test module for grid reading."""
+def test_dummy():
+    """Dummy test."""
+    assert 1
